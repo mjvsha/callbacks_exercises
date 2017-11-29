b803ca003b9dd07b1a71a0a8e28976a12275db94
Meera <mjvsharma@outlook.com>
2017-11-29 16:13:50 -0500

    I FINALLY SUMMED THE SALES!!!!!! AHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- main.js
+++ main.js
@@ -135,7 +135,7 @@ function sales() {
 
     var counter = 0;
 
-    for (var i = 0; i < transactions.length; i++) {
+    for (var i = 0; i < totalTransactions; i++) {
       if (transactions[i]['type'] === 'sale') {
         counter +=1;
     }
@@ -143,7 +143,19 @@ function sales() {
     return counter;
 
 }
-var numSales = sales();
+
+
+// var numSales = transactionSales.length;
+
+var transactionSales = transactions.filter(function(transaction) {
+  return (transaction.type === 'sale')
+});
+
+function typeSale(array) {
+  return array.type === 'sale';
+}
+
+var numSales = transactions.filter(typeSale).length
 
 //why does this way work? when i assign the fuction directly, i dont get the return value but the entire function itself
 
@@ -179,22 +191,30 @@ console.log( 'The total number of sales is:', numSales );
   Calculate the total number of 'purchases'.
 */
 
-function purchases() {
-  var counter = 0;
-  for (var i = 0; i < transactions.length; i++) {
-    if (transactions[i]['type'] === 'purchase')
-      counter +=1;
-  }
-    return counter;
-}
+// function purchases() {
+//   var counter = 0;
+//   for (var i = 0; i < totalTransactions; i++) {
+//     if (transactions[i]['type'] === 'purchase')
+//       counter +=1;
+//   }
+//     return counter;
+// }
+//
+// function typePurchase(array) {
+//   return (array.type === 'purchase');
+// }
+//
+
+// var numPurchases = transactions.filter(typePurchase).length;
+
+var numPurchases = transactions.filter(function(array) {
+    return(array.type === 'purchase');
+});
 
 
-var numPurchases = purchases();
 
 
-
-
-console.log( 'The total number of purchases is:', numPurchases );
+console.log( 'The total number of purchases is:', numPurchases.length );
 
 
 // --------------------------------------------------
@@ -206,7 +226,16 @@ console.log( 'The total number of purchases is:', numPurchases );
   HINT(S):
   - Don't forget that 'purchases' can also be made in 'cash'!
 */
-var numCashSales;
+
+//
+// function cashSale(array) {
+//   return (array.type === 'sale' && array.paymentMethod === 'cash');
+// }
+
+
+var numCashSales = transactions.filter(function(array) {
+  return (array.type === 'sale' && array.paymentMethod === 'cash');
+}).length;
 
 console.log( 'The total number of cash sales is:', numCashSales );
 
@@ -220,7 +249,15 @@ console.log( 'The total number of cash sales is:', numCashSales );
   HINT(S):
   - Make sure to exclude any 'sales' made by 'credit'!
 */
-var numCreditPurchases;
+
+
+  // function creditPurchase(array) {
+  //   return (array.type === 'purchase' && array.paymentMethod === 'credit')
+  // }
+
+var numCreditPurchases = transactions.filter(function(array) {
+  return (array.type === 'purchase' && array.paymentMethod === 'credit')
+}).length;
 
 console.log( 'The total number of credit purchases is:', numCreditPurchases );
 
@@ -237,7 +274,61 @@ console.log( 'The total number of credit purchases is:', numCreditPurchases );
   - The assembled array should be made up of strings, not full `transaction` objects.
   - This array is allowed to contain duplicate values.
 */
-var uniqueVendors;
+
+  var purchases = transactions.filter(function(array) {
+    return (array.type === 'purchase');
+  })
+
+  var uniqueVendors = purchases.map(function(array) {
+        return array.vendor;
+
+    });
+
+  //
+  // for (var i = 0; i < transactions.length; i++) {
+  //   if (transactions[i]['vendor'])
+  //     vendorsArray.push(transactions[i]['vendor']);
+  // }
+  //
+  // function findVendor(array) {
+  //   return array.vendor;
+  // }
+  // var tryOut = transactions.filter(findVendor) {
+  //
+  // }
+
+
+
+  // function findVendor(array) {
+  //   return array.vendor
+  // }
+  //
+  //
+  // vendorsArray = [];
+  //
+  // function vendors(array) {
+  //   for (var i = 0; i < array.length; i++) {
+  //     if (array.vendor !== undefined)
+  //       vendorsArray.push(array.vendor);
+  //       console.log(array.vendor);
+  //   }
+  //
+  // }
+
+
+  //this will give me an array of objects that have vendors
+  //now i have to take this array, and extract the vendors from each one of them
+
+
+
+
+
+  // function findVendor(array) {
+  //   for (var i =0; i < array.length; i++) {
+  //     console.log(array[i].vendor);
+  //   }
+  // }
+
 
 console.log( 'The unique vendors are:', uniqueVendors );
 
@@ -254,7 +345,16 @@ console.log( 'The unique vendors are:', uniqueVendors );
   - The assembled array should be made up of strings, not full `transaction` objects.
   - Make sure that the resulting array *does not* include any duplicates.
 */
-var uniqueCustomers;
+
+var customers = transactions.filter(function(array) {
+
+  return (array.type === 'sale');
+
+});
+
+var uniqueCustomers = customers.map(function(array) {
+  return (array.customer);
+});
 
 console.log( 'The unique customers are:', uniqueCustomers );
 
@@ -272,7 +372,23 @@ console.log( 'The unique customers are:', uniqueCustomers );
   - There may be more than 1 'sale' that includes 5 or more items.
   - Individual transactions do not have either `name` or `numItems` properties, we'll have to add them to the output.
 */
-var bigSpenders;
+
+//look for the sales, so get an array that includes only the sales , we have a customers array
+//then loop through the transactions, and count if greater than 5
+//if greater than 5, we should return the transactions
+
+
+
+
+var bigSpenders = customers.filter(function(array) {
+
+return (array.items.length >= 5);
+
+});
+
+
+
+
 
 console.log( 'The "big spenders" are:', bigSpenders );
 
@@ -281,14 +397,63 @@ console.log( 'The "big spenders" are:', bigSpenders );
 // QUESTION 08
 // --------------------------------------------------
 /*
-  Calculate the sum of the *first* 'sale' transaction.
+  Calculate the sum of all of the 'sales' transactions.
 
   HINT(S):
   - Transactions don't have 'prices', but their 'items' do!
 */
-var sumSales;
 
-console.log( 'The sum of all sales is:', sumSales );
+//map, goes theough the elements of the array and then applies function on them, returning new values
+//filters values based on true/falseness of them
+//reduce, reduces the input into a number
+
+//i want to filter the array for ll the sales transactions
+//i want to iterate through the items array and grab all the sale prices
+//and then i want to reduce the array into a sum of all the sales
+
+//how the fucj do u iterate through the array and grab the items??
+//do i need to make a separate for loop to itrate through the items array?
+
+
+var sumSales1 = transactions.filter(function(array){
+  return (array.type === 'sale');
+});
+
+
+
+
+console.log('HOHOHOH')
+
+
+
+var sumTheSales = function(array){
+  var salesPriceArray = [];
+
+  sumSales1.forEach(function(element){
+
+    for (var i = 0; i < element['items'].length; i++) {
+      salesPriceArray.push((element['items'][i]['price']));
+      // console.log(salesPriceArray)
+    };
+  });
+
+  return salesPriceArray.reduce(function(total, current, index){
+    // console.log(total+current);
+    return total += current;
+    // return total;
+
+  });
+
+  // console.log(salesPriceTotal);
+  //reduce takes a function and the starting value as the parameter
+}
+
+
+// console.log(lol(sumSales1))
+
+
+
+console.log( 'The sum of all sales is: $', sumTheSales(sumSales1));
 
 
 // --------------------------------------------------
